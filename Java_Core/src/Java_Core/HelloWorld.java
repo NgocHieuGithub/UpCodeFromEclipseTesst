d83a05af13b24f0e2b9d13955b1dbe236d5222cb
ngocHieuGH <ADMIN@MSI>
2024-05-17 13:07:12 +0530

    new update

--- Java_Core/src/Java_Core/HelloWorld.java
+++ Java_Core/src/Java_Core/HelloWorld.java
@@ -3,6 +3,7 @@ package Java_Core;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		System.out.println("HelloWord");
 		// TODO Auto-generated method stub
 
